04ca4f6ae80989e7927d87f246c16fff4fd049cb
Alenor <alenor96@gmail.com>
2017-03-21 12:20:31 +0300

    Lab DependencyInjection

---
+++ src/ApplicationContext.java
@@ -0,0 +1,23 @@
+import Annotation.Autowired;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Alenor on 14.03.2017.
+ */
+public class ApplicationContext {
+
+    private static ApplicationContext instance;
+
+    public static ApplicationContext getInstance() {
+        return instance;
+    }
+
+    static {
+        instance = new ApplicationContext();
+    }
+
+    public Object getBean(String beanName) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        return BeanFactory.createBean(beanName);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import Objects.*;
+
+/**
+ * Created by Alenor on 10.03.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = ApplicationContext.getInstance();
+        try {
+            Car car = (Car) context.getBean("Objects.Car");
+            Engine engine = car.getEngine();
+            engine.setModel("sas");
+
+            Plain plain = (Plain) context.getBean("Objects.Plain");
+            PlainEngine plainEngine = plain.getPlainEngine();
+            plainEngine.setModel("asd");
+            PlainEngineProperties prop = plainEngine.getPlainEngineProperties();
+            prop.setFuelCapacity(123);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
